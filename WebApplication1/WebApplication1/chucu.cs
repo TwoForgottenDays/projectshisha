5c861fdfab86b507aa4d6633b8e14a51dc2ba632
TwuForgottenDays <jossueolmos1005@gmail.com>
2025-07-01 20:34:15 +0200

    ta raro el asunto

--- WebApplication1/WebApplication1/chucu.cs
+++ WebApplication1/WebApplication1/chucu.cs
@@ -1,2 +1,23 @@
-decimal measurement = 123456.78912m;
-Console.WriteLine($"Measurement: {measurement:N} units");
+for (int i = 1; i >= 0; i++) 
+{
+    
+    if ( i % 3 == 0) 
+    {
+        Console.WriteLine("Es robsito: " + i);
+        continue; // Skip the rest of the loop for even numbers
+    }
+    {
+        if (i % 5 != 0)
+        {
+            Console.WriteLine("Es calvo: " + i);
+        }
+        if (i % 7 == 0)
+        {
+            Console.WriteLine("Es calvo y robosito: " + i);
+        }
+
+        if (i == 100)
+
+                break;
+    }
+}
--- WebApplication1/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb1bee97b3b15d9d66f666ccc618b9f5a944734d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
