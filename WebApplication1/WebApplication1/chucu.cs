efccd0a3b8537e190939befcd3851825d4ffc9e8
TwuForgottenDays <jossueolmos1005@gmail.com>
2025-07-08 21:27:12 +0200

    namas pa que me deje lptm

--- WebApplication1/WebApplication1/chucu.cs
+++ WebApplication1/WebApplication1/chucu.cs
@@ -1,45 +1,12 @@
-string? readResult;
-int startIndex = 0;
-bool goodEntry = false;
+int a = 3;
+int b = 4;
+int c = 0;
 
-int[] numbers = { 1, 2, 3, 4, 5 };
+Multiply(a, b, c);
+Console.WriteLine($"global statement: {a} x {b} = {c}");
 
-// Display the array to the console.
-Console.Clear();
-Console.Write("\n\rThe 'numbers' array contains: { ");
-foreach (int number in numbers)
+void Multiply(int a, int b, int c) 
 {
-    Console.Write($"{number} ");
-}
-
-// To calculate a sum of array elements, 
-//  prompt the user for the starting element number.
-Console.WriteLine($"}}\n\r\n\rTo sum values 'n' through 5, enter a value for 'n':");
-while (goodEntry == false)
-{
-    readResult = Console.ReadLine();
-    goodEntry = int.TryParse(readResult, out startIndex);
-
-    if (startIndex > 5)
-    {
-        goodEntry = false;
-        Console.WriteLine("\n\rEnter an integer value between 1 and 5");
-    }
-}
-
-// Display the sum and then pause.
-Console.WriteLine($"\n\rThe sum of numbers {startIndex} through {numbers.Length} is: {SumValues(numbers, startIndex)}");
-
-Console.WriteLine("press Enter to exit");
-readResult = Console.ReadLine();
-
-// This method returns the sum of elements n through 5
-static int SumValues(int[] numbers, int n)
-{
-    int sum = 0;
-    for (int i = n; i < numbers.Length; i++)
-    {
-        sum += numbers[i];
-    }
-    return sum;
+    c = a * b;
+    Console.WriteLine($"inside Multiply method: {a} x {b} = {c}");
 }
