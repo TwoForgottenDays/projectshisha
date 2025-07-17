207cb2b0e04aefe5ac9aacba33defc7b7d5af1e7
TwuForgottenDays <jossueolmos1005@gmail.com>
2025-07-17 21:03:18 +0200

    mejorando

--- WebApplication1/WebApplication1/chucu.cs
+++ WebApplication1/WebApplication1/chucu.cs
@@ -1,12 +1,4 @@
-int a = 3;
-int b = 4;
-int c = 0;
-
-Multiply(a, b, c);
-Console.WriteLine($"global statement: {a} x {b} = {c}");
-
-void Multiply(int a, int b, int c) 
+int Sumar(int a, int b)
 {
-    c = a * b;
-    Console.WriteLine($"inside Multiply method: {a} x {b} = {c}");
+    return a + b;
 }
